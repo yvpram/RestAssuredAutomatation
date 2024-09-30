c89bb93dfb84d2694a916290a433aeb48cd5f042
varaprasadram1 <prasad.ram.vara.y@gmail.com>
2024-09-30 13:12:19 +0530

    base files

---
+++ src/main/java/req/respo/petshop/HttpResponses.java
@@ -0,0 +1,27 @@
+package req.respo.petshop;
+
+import io.restassured.RestAssured;
+
+public class HttpResponses {
+	
+	public static void get() {
+		RestAssured rs = new RestAssured();
+	}
+
+	public static void put() {
+
+	}
+
+	public static void post() {
+
+	}
+
+	public static void pach() {
+
+	}
+
+	public static void delete() {
+
+	}
+
+}
